969d51ab3af96026efbab97d8ee5d34b2684c2a0
Andrzej Burlinski <128097055+Revoxer@users.noreply.github.com>
2026-06-07 14:55:51 +0200

    morgan config

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,11 +4,13 @@ import cors from "cors";
 import authRoutes from "./routes/auth.routes";
 import { errorHandler } from "./middleware/error.middleware";
 import { notFound } from "./middleware/notFound.middleware";
+import morgan from "morgan";
 
 const app: Application = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(morgan("dev"));
 
 app.use("/api/v1/auth", authRoutes);
 
